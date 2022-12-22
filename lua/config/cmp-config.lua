c277fd7b23804ff9d11a8fd303e3172aa418e849
Richard McCulloch <richard@linuxbox.com.au>
2022-12-22 10:24:22 +0800

    Enables path source for nvim cmdline

--- lua/config/cmp-config.lua
+++ lua/config/cmp-config.lua
@@ -153,10 +153,10 @@ cmp.setup({
 -- })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
--- cmp.setup.cmdline(':', {
---     sources = cmp.config.sources({
---     { name = 'path' }
---     }, {
---         { name = 'cmdline' }
---         })
--- })
+cmp.setup.cmdline(':', {
+    sources = cmp.config.sources({
+    { name = 'path' }
+    }, {
+        { name = 'cmdline' }
+        })
+})
